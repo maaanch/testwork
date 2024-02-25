9253f53f552349fd0c55390d7b5bad2b011ce7f4
salman <sashraf@technologyrivers.com>
2024-02-25 19:56:24 +0500

    front end setup strt

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Dashboard from "./Main/Dashboard";
 
 function App() {
   return (
-    < >
-      <h1>React app</h1>
+    <>
+      <Dashboard />
     </>
   );
 }
